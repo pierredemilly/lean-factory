34f7a812763dac050fa5d836d5671a2401038dca
pierredemilly <pierre.de.milly@gmail.com>
2013-11-02 17:02:00 +0100

    Basic website

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-
-  <div class="twelve columns">
-    <h1>Base template for Wordpress</h1>
-    <p>by Pierre de Milly - http://lanuagerie.com</p>
-  </div>
-
-<?php get_footer(); ?>
